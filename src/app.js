76463a09c805cfb57422e5601d45202dc58bede2
Snofts <samuelsnofts@gmail.com>
2025-06-25 09:00:15 +0100

    Fix: parse PORT env var correctly

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT || 3000, () =>
+    app.listen(parseInt(process.env.PORT, 10) || 3000, () =>
       console.log(`Server running on port ${process.env.PORT}`)
     );
   })
